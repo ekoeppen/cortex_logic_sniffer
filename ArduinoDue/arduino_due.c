38a0a70d3188cd40887877362184634cfaf784d7
Eckhart Koeppen <eck@40hz.org>
2013-03-02 15:41:11 +0200

    More Arduino support

--- ArduinoDue/arduino_due.c
+++ ArduinoDue/arduino_due.c
@@ -1,12 +1,22 @@
 #include "sam3x8e.h"
+#include "pmc/pmc.h"
+#include "pio/pio.h"
+#include "pio/pio_sam3x8e.h"
+#include "uart/uart.h"
 
 extern volatile uint32_t reset_control;
 
 int32_t usart1_rcv_char = -1;
 int32_t usart2_rcv_char = -1;
 
+void init_board(void)
+{
+    SystemInit();
+}
+
 void init_led(void)
 {
+    pio_configure(PIOB, PIO_OUTPUT_0, PIO_PB27, PIO_DEFAULT) ;
 }
 
 void init_button(void)
@@ -15,10 +25,24 @@ void init_button(void)
 
 void init_log(void)
 {
+    struct sam_uart_opt uart_opt = {
+	.ul_baudrate = 115200,
+	.ul_mode = UART_MR_CHMODE_NORMAL,
+	.ul_chmode = UART_MR_PAR_NO
+    };
+
+    uart_opt.ul_mck = SystemCoreClock;
+    pmc_enable_periph_clk(ID_UART);
+    uart_init(UART, &uart_opt);
+    uart_enable_tx(UART);
 }
 
 void log_string(char *str)
 {
+    while (*str != '\0') {
+        uart_write(UART, *str);
+        str++;
+    }
 }
 
 void log_nl()
@@ -32,6 +56,7 @@ void log_hex(uint32_t n)
 
 void set_led(uint32_t on)
 {
+    pio_set_output(PIOB, PIO_PB27, on, 0, PIO_PULLUP);
 }
 
 void init_uart(void)
